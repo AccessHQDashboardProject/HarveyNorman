31ead87ba5b06e0a13b3a8880b4a8706fea02243
accesshq <swati.gupta@accesshq.com>
2017-07-06 09:48:07 +1000

    added Test file for Git testing.

---
+++ src/test/java/PaymentE2E/TestClass.java
@@ -0,0 +1,11 @@
+package PaymentE2E;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Testing GitHub...");
+	}
+
+}
